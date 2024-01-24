7b7819582fe6786553abb86487c9215d59bf7751
Alex <a.ofori@rocketmail.com>
2024-01-24 15:54:00 +0000

    First flipbook commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <flipbook class="flipbook" :pages="['array', 'of', 'image', 'URLs']" v-slot="flipbook">
+      <button @click="flipbook.flipLeft">Previous Page</button>
+      <button @click="flipbook.flipRight">Next Page</button>
+    </flipbook>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Flipbook from 'flipbook-vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Flipbook },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.flipbook {
+  
+  width: 90vw;
+  height: 90vh;
 }
 </style>
+
